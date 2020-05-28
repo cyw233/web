7b5ba2e75a5e9d244267d7f82282f7cb8e460b09
Colin Wang <chenyangw233@gmail.com>
2020-05-28 12:35:59 +1000

    Finish 155

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-import axios from 'axios';
+import { User } from './models/User';
 
-// axios.post('http://localhost:3000/users', {
-//   name: 'myname',
-//   age: 20,
-// });
+const user = new User({ name: 'new record', age: 0 });
 
-axios.get('http://localhost:3000/users/1');
+user.events.on('change', () => {
+  console.log('change!');
+});
+
+user.events.trigger('change');
